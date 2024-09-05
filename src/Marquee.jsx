476e351ac2f223687c2b4f757714965a1bcd628e
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-09-06 01:53:25 +0530

    bkdfbjs

--- src/Marquee.jsx
+++ src/Marquee.jsx
@@ -12,56 +12,56 @@ function Marquee() {
   const [Techwidth, setTechStack] = useState(0);
 
   const techStackItems = [
-    { component: <FaHtml5 className="w-[10vw] sm:w-[8vw]" />, key: "html5" },
-    { component: <FaCss3 className="w-[10vw] sm:w-[8vw]" />, key: "css3" },
-    { component: <FaJs className="w-[10vw] sm:w-[8vw]" />, key: "js" },
-    { component: <FaReact className="w-[10vw] sm:w-[8vw]" />, key: "react" },
+    { component: <FaHtml5 className="w-[13vw] sm:w-[8vw]" />, key: "html5" },
+    { component: <FaCss3 className="w-[13vw] sm:w-[8vw]" />, key: "css3" },
+    { component: <FaJs className="w-[13vw] sm:w-[8vw]" />, key: "js" },
+    { component: <FaReact className="w-[13vw] sm:w-[8vw]" />, key: "react" },
     {
-      component: <RiTailwindCssFill className="w-[10vw] sm:w-[8vw]" />,
+      component: <RiTailwindCssFill className="w-[13vw] sm:w-[8vw]" />,
       key: "tailwind",
     },
     {
-      component: <GiSteamLocomotive className="w-[10vw] sm:w-[8vw]" />,
+      component: <GiSteamLocomotive className="w-[13vw] sm:w-[8vw]" />,
       key: "locomotive",
     },
     {
-      component: <SiGreensock className="w-[10vw] sm:w-[8vw]" />,
+      component: <SiGreensock className="w-[13vw] sm:w-[8vw]" />,
       key: "greensock",
     },
     {
       component: (
-        <img className="w-[10vw] sm:w-[8vw]" src="/lenis svg.svg" alt="Lenis" />
+        <img className="w-[13vw] sm:w-[8vw]" src="/lenis svg.svg" alt="Lenis" />
       ),
       key: "lenis",
     },
     {
-      component: <PiFramerLogo className="w-[10vw] sm:w-[8vw]" />,
+      component: <PiFramerLogo className="w-[13vw] sm:w-[8vw]" />,
       key: "Framers",
     },
-    { component: <FaHtml5 className="w-[6vw] sm:w-[8vw]" />, key: "html51" },
-    { component: <FaCss3 className="w-[6vw] sm:w-[8vw]" />, key: "css32" },
-    { component: <FaJs className="w-[6vw] sm:w-[8vw]" />, key: "js2" },
-    { component: <FaReact className="w-[6vw] sm:w-[8vw]" />, key: "reac2t" },
+    { component: <FaHtml5 className="w-[13vw] sm:w-[8vw]" />, key: "html51" },
+    { component: <FaCss3 className="w-[13vw] sm:w-[8vw]" />, key: "css32" },
+    { component: <FaJs className="w-[13vw] sm:w-[8vw]" />, key: "js2" },
+    { component: <FaReact className="w-[13vw] sm:w-[8vw]" />, key: "reac2t" },
     {
-      component: <RiTailwindCssFill className="w-[6vw] sm:w-[8vw]" />,
+      component: <RiTailwindCssFill className="w-[13vw] sm:w-[8vw]" />,
       key: "tailwind2",
     },
     {
-      component: <GiSteamLocomotive className="w-[6vw] sm:w-[8vw]" />,
+      component: <GiSteamLocomotive className="w-[13vw] sm:w-[8vw]" />,
       key: "locomotive2",
     },
     {
-      component: <SiGreensock className="w-[6vw] sm:w-[8vw]" />,
+      component: <SiGreensock className="w-[13vw] sm:w-[8vw]" />,
       key: "greensock2",
     },
     {
       component: (
-        <img className="w-[6vw] sm:w-[8vw]" src="/lenis svg.svg" alt="Lenis" />
+        <img className="w-[13vw] sm:w-[8vw]" src="/lenis svg.svg" alt="Lenis" />
       ),
       key: "lenis2",
     },
     {
-      component: <PiFramerLogo className="w-[6vw] sm:w-[8vw]" />,
+      component: <PiFramerLogo className="w-[13vw] sm:w-[8vw]" />,
       key: "Framers2",
     },
   ];
