1f924f43df315b2bfd99b2ed38b7136796e1b499
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-09-06 02:02:43 +0530

    sdvcxz

--- src/Menu1.jsx
+++ src/Menu1.jsx
@@ -43,10 +43,10 @@ function Menu1({ change }) {
   };
 
   return (
-    <motion.div className=" absolute z-[900] top-0 left-0 blur-my h-[200vh] ">
-      <motion.div className="sticky top-0  left-0 h-screen w-screen  justify-center gap-[-3vh]  flex  sm:justify-start sm:flex-row flex-wrap  items-center">
+    <motion.div className=" relative z-[900] top-0 left-0 blur-my h-[400vh] ">
+      <motion.div className="sticky top-0 left-0 justify-center blur-my flex   sm:justify-start sm:flex-row flex-wrap gap-5  items-start">
         {/* 3D Canvas Component */}
-        <div className=" flex  justify-start items-center w-screen h-[45vh] lg:w-1/2 lg:h-screen sm:h-1/3">
+        <div className=" flex  justify-start items-center w-screen h-[55vh] lg:w-1/2 lg:h-screen sm:h-1/3">
           <Suspense
             fallback={
               <div className="flex justify-center items-center text-center  text-lg">
@@ -59,7 +59,7 @@ function Menu1({ change }) {
         </div>
 
         {/* Menu Options */}
-        <div className="flex flex-col justify-center items-center text-center lg:items-end  lg:pr-20 gap-6 lg:gap-10 w-auto lg:w-1/2 h-auto">
+        <div className="flex flex-col justify-center items-center text-center lg:items-end   lg:pr-20 gap-6 lg:gap-10 w-auto lg:w-1/2 h-auto">
           {menuOptions.map((item) => (
             <motion.span
               onClick={() => handleMenuClick(item.Navlink)}
