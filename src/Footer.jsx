19a0601fd48c90724662dc4399137a09813996ed
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-09-06 16:40:45 +0530

    footer Responive impored

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,22 +5,40 @@ import { GoArrowUpRight } from "react-icons/go";
 function Footer() {
   return (
     <div className="bg-[#0a0908] mt-14 border-y-2 mb-10 border-white/50 text-[5vw] md:text-[2.5vw] lg:text-[1.5vw] Satoshi flex flex-col md:flex-row flex-wrap leading-tight">
-      <div className="flex text-[8vw] md:text-[4vw] lg:text-[2.5vw] w-full md:w-screen flex-wrap ">
-        <div className="hover:text-red-500 border-r-2 w-full md:w-1/4 flex flex-row gap-5 h-full items-center justify-center transition-all duration-300">
-          <span>Gmail</span>
-          <GoArrowUpRight className="inline-block transition-transform transform hover:-translate-y-4 hover:translate-x-4" />
+      <div className="flex text-[8vw] md:text-[4vw] lg:text-[2.5vw] w-full md:w-screen flex-wrap mx-auto ">
+        <div className="hover:text-red-500  border-r-2 w-full mx-auto md:w-1/4 flex flex-row gap-5 h-full  transition-all duration-300">
+          <span className="mx-auto my-auto ">
+            Gmail{" "}
+            <GoArrowUpRight className="inline-block mx-auto transition-transform transform hover:-translate-y-4 hover:translate-x-4" />
+          </span>
         </div>
-        <div className="hover:text-blue-500 border-r-2 w-full md:w-1/4 flex flex-row gap-5 h-full items-center justify-center transition-all duration-300">
-          <span>LinkedIn</span>
-          <GoArrowUpRight className="inline-block transition-transform transform hover:-translate-y-4 hover:translate-x-4" />
+        <div className="hover:text-purple-500  border-r-2  w-full md:w-1/4 flex flex-row gap-5 mx-auto h-full  transition-all duration-300">
+          <span
+            className="
+          mx-auto my-auto"
+          >
+            GitHub{" "}
+            <GoArrowUpRight className="inline-block w-1/5 transition-transform transform hover:-translate-y-4 hover:translate-x-4" />
+          </span>
         </div>
-        <div className="hover:text-purple-500 w-full md:w-1/4 flex flex-row gap-5 h-full items-center justify-center transition-all duration-300">
-          <span>GitHub</span>
-          <GoArrowUpRight className="inline-block w-1/5 transition-transform transform hover:-translate-y-4 hover:translate-x-4" />
+        <div className="hover:text-blue-500  w-full mx-auto md:w-1/4 flex flex-row gap-5 h-full  transition-all duration-300">
+          <span
+            className="
+          mx-auto my-auto"
+          >
+            LinkedIn{" "}
+            <GoArrowUpRight className="inline-block transition-transform transform hover:-translate-y-4 hover:translate-x-4" />
+          </span>
         </div>
-        <div className="flex w-full md:w-1/4 flex-col border-l-2 text-center justify-center gap-5 py-5 h-full">
-          <FlipLink>My Resume</FlipLink>
-          <FlipLink>About Me</FlipLink>
+
+        <div className="flex w-full md:w-1/4 flex-col border-l-2  mx-auto  gap-5  h-full">
+          <span className="mx-auto my-auto">
+            {" "}
+            <FlipLink>My Resume</FlipLink>
+          </span>
+          <span className="mx-auto my-auto">
+            <FlipLink>About Me</FlipLink>
+          </span>
         </div>
       </div>
     </div>
